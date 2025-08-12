ebb0c30c5e7caece28dad21bfec28809c236b2b0
NiazSagor <niazsagor@gmail.com>
2025-08-12 10:44:59 +0600

    :zap: Improve result screen UI
    - The question index is now displayed in a colored circle (blue for correct, purple for incorrect).
    - Question text is now white.
    - User answer text is now purple.
    - Correct answer text is now blue.
    - Added spacing between the question index and the question details.
    - Aligned question details to the start.
    
    Took 25 minutes

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -12,16 +12,41 @@ class QuestionsSummary extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
+            bool isCorrect = (data["user_answer"] == data["correct_answer"]);
             return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(((data["question_index"] as int) + 1).toString()),
+                Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: isCorrect ? Colors.blue : Colors.purpleAccent,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      ((data["question_index"] as int) + 1).toString(),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(data["question"] as String),
+                      Text(
+                        data["question"] as String,
+                        style: TextStyle(color: Colors.white),
+                      ),
                       const SizedBox(height: 5),
-                      Text(data["user_answer"] as String),
-                      Text(data["correct_answer"] as String),
+                      Text(
+                        data["user_answer"] as String,
+                        style: TextStyle(color: Colors.purpleAccent),
+                      ),
+                      Text(
+                        data["correct_answer"] as String,
+                        style: TextStyle(color: Colors.blue),
+                      ),
                     ],
                   ),
                 ),
